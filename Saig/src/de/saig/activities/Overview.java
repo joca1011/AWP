7d5a435db22ec895e13db31f177c48208a82939b
Steffen Pable <past1011(at)HS-Karlsruhe.de>
2013-11-20 19:09:09 +0100

    overview

---
+++ Saig/src/de/saig/activities/Overview.java
@@ -0,0 +1,18 @@
+package de.saig.activities;
+
+import de.awp.saig.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Overview extends Activity {
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_overview);
+		
+		
+	}
+}
