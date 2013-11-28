a993b2345f9fda020239a5110573f228753d7609
Steffen Pable <past1011(at)HS-Karlsruhe.de>
2013-11-28 13:35:00 +0100

    Saig - Evernote Verbindung realisiert

--- Saig/src/de/saig/activities/Overview.java
+++ Saig/src/de/saig/activities/Overview.java
@@ -33,4 +33,12 @@ public class Overview extends Activity {
 	    
 		
 	}
+	
+	public void onClickEvernoteButton (View view) {
+		//TODO validate login
+		Intent LaunchIntent = getPackageManager().getLaunchIntentForPackage("com.evernote.android.sample");
+		startActivity(LaunchIntent);
+	    
+		
+	}
 }
