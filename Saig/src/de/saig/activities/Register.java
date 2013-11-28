c50dbe5f4447d207aa2f2b6acb2105a4318d505d
Steffen Pable <past1011(at)HS-Karlsruhe.de>
2013-11-28 11:35:33 +0100

    Verlinkung activity_register mit Register.java

--- Saig/src/de/saig/activities/Register.java
+++ Saig/src/de/saig/activities/Register.java
@@ -2,7 +2,9 @@ package de.saig.activities;
 
 import de.awp.saig.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends Activity{
 	
@@ -10,6 +12,16 @@ public class Register extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
+		
+	}
+	
+	public void signInClick (View view) {
+		
+		
+		//TODO Hier ist vorerst eine Verlinkung zurück. Die Restrierung muuss hier implementiert werden.
+		Intent intent = new Intent(this, Login.class);
+	    startActivity(intent);
+	    
 		
 	}
 	
