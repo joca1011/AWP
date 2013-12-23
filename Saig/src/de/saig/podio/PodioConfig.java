c9825e407e0e680ae530e512f608333fb9bd67f0
Devon Le Duke <lede1011@hs-karlsruhe.de>
2013-12-23 13:02:59 +0100

    jetzt aber

--- Saig/src/de/saig/podio/PodioConfig.java
+++ Saig/src/de/saig/podio/PodioConfig.java
@@ -2,7 +2,7 @@ package de.saig.podio;
 
 public class PodioConfig {
 	
-	static String host = "http://pbl-mig.financialecology.info";
+	static String host = "pbl-mig.financialecology.info";
 	
 	
 }
