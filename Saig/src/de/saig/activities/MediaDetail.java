6c65eacd49ac0a6e4c3e7993621f1ff4d77f5172
Peter Grittner <grpe1011@hs-karlsruhe.de>
2014-01-12 03:57:43 +0100

    Settings Games usw.

--- Saig/src/de/saig/activities/MediaDetail.java
+++ Saig/src/de/saig/activities/MediaDetail.java
@@ -2,7 +2,9 @@ package de.saig.activities;
 
 import de.awp.saig.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MediaDetail extends Activity{
 
@@ -10,7 +12,13 @@ public class MediaDetail extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_media_detail);
-		
-		
 	}
+	
+	public void save(View view) {
+		//TODO validate login
+		Intent intent = new Intent(this, Overview.class);
+	    startActivity(intent);
+	}
+	
+	
 }
